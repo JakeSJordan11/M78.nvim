29c63f99caea24788dbb2d47aa85255b801a295f
jake jordan <jakesjordan11@gmail.com>
2022-02-02 11:43:38 -0700

    removed media files extension

--- lua/jake/telescope.lua
+++ lua/jake/telescope.lua
@@ -96,5 +96,12 @@ telescope.setup({
 		--   extension_config_key = value,
 		-- }
 		-- please take a look at the readme of the extension you want to configure
+
+		-- media_files = {
+		-- 	-- filetypes whitelist
+		-- 	-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+		-- 	filetypes = { "png", "webp", "jpg", "jpeg" },
+		-- 	find_cmd = "rg", -- find command (defaults to `fd`)
+		-- },
 	},
 })

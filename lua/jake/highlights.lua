5d568f3b8ba173f9d7e5c49b08bbf93224a64625
jake jordan <jakesjordan11@gmail.com>
2022-02-02 11:01:22 -0700

    added highlights file for new look

---
+++ lua/jake/highlights.lua
@@ -0,0 +1,24 @@
+-- vim.cmd("highlight Normal ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight NormalNC ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight CursorLine ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight CursorLineNC ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight CursorLineNr ctermfg=NONE ctermbg=NONE guibg=NONE")
+-- vim.cmd("highlight SignColumn ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight SignColumnNC ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight EndOfBuffer ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight EndOfBufferNC ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight BufferCurrentMod ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight BufferCurrentSelection ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight ColorColumn ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight LineNr ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+-- vim.cmd("highlight GitSignsChange ctermbg=NONE guibg=NONE")
+-- vim.cmd("highlight GitSignsAdd ctermbg=NONE guibg=NONE")
+-- vim.cmd("highlight Comment ctermbg=NONE guibg=NONE") -- remove comment bg highlights
+-- vim.cmd("highlight DiagnosticWarn ctermbg=NONE guibg=NONE")
+-- vim.cmd("highlight DiagnosticHint ctermbg=NONE guibg=NONE")
+-- vim.cmd("highlight DiagnosticInfo ctermbg=NONE guibg=NONE")
+-- vim.cmd("highlight DiagnosticError ctermbg=NONE guibg=NONE")
+
+
+-- vim.cmd("highlight BufferLineBackground ctermbg=NONE guibg=red") -- Background of tabs not including icons and empty bar
+

be08e3fa9bc055514430d2c34380173a7b17c0e3
jake jordan <jakesjordan11@gmail.com>
2022-02-02 10:59:22 -0700

    format spacing

--- lua/jake/colorscheme.lua
+++ lua/jake/colorscheme.lua
@@ -1,21 +1,21 @@
---local colorscheme = "none"
---local colorscheme = "ayu"
---local colorscheme = "bogster"
---local colorscheme = "boo"
---local colorscheme = "catppuccin"
---local colorscheme = "everforest"
+-- local colorscheme = "none"
+-- local colorscheme = "ayu"
+-- local colorscheme = "bogster"
+-- local colorscheme = "boo"
+-- local colorscheme = "catppuccin"
+-- local colorscheme = "everforest"
 local colorscheme = "gruvbox"
---local colorscheme = "halcyon"
---local colorscheme = "lvim"
---local colorscheme = "melange"
---local colorscheme = "night-owl"
---local colorscheme = "rvcs"
---local colorscheme = "tokyonight"
---vim.api.nvim_set_var(colorscheme .. "_italic_comments", true)
---vim.api.nvim_set_var(colorscheme .. "_italic_keywords", true)
---vim.api.nvim_set_var(colorscheme .. "_italic_functions", true)
---vim.api.nvim_set_var(colorscheme .. "_sidebars", { "qf", "vista_kind", "terminal", "packer" })
---vim.api.nvim_set_var(colorscheme .. "_style", "night")
+-- local colorscheme = "halcyon"
+-- local colorscheme = "lvim"
+-- local colorscheme = "melange"
+-- local colorscheme = "night-owl"
+-- local colorscheme = "rvcs"
+-- local colorscheme = "tokyonight"
+vim.api.nvim_set_var(colorscheme .. "_italic_comments", true)
+vim.api.nvim_set_var(colorscheme .. "_italic_keywords", true)
+vim.api.nvim_set_var(colorscheme .. "_italic_functions", true)
+vim.api.nvim_set_var(colorscheme .. "_sidebars", { "qf", "vista_kind", "terminal", "packer" })
+-- vim.api.nvim_set_var(colorscheme .. "_style", "night")
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 

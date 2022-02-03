881c93a1e5837ce9a2784dce5b1b21105add3743
jake jordan <jakesjordan11@gmail.com>
2022-02-03 16:49:32 -0700

    added octo gh plugin

--- lua/jake/plugins.lua
+++ lua/jake/plugins.lua
@@ -151,6 +151,18 @@ return packer.startup(function(use)
 	-- Git
 	use("lewis6991/gitsigns.nvim")
 
+	use({
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"kyazdani42/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup()
+		end,
+	})
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then

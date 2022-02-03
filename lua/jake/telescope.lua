ba0995c55207952c97b25043ee611b145d127c17
jake jordan <jakesjordan11@gmail.com>
2022-02-03 16:49:17 -0700

    added fzf extension to telexscope

--- lua/jake/telescope.lua
+++ lua/jake/telescope.lua
@@ -96,12 +96,6 @@ telescope.setup({
 		--   extension_config_key = value,
 		-- }
 		-- please take a look at the readme of the extension you want to configure
-
-		-- media_files = {
-		-- 	-- filetypes whitelist
-		-- 	-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-		-- 	filetypes = { "png", "webp", "jpg", "jpeg" },
-		-- 	find_cmd = "rg", -- find command (defaults to `fd`)
-		-- },
 	},
 })
+telescope.load_extension("fzf")

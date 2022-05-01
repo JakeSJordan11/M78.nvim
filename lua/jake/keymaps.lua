137eb3de5b58fdd931a14e44b927612274ae7002
jake jordan <jakesjordan11@gmail.com>
2022-05-01 10:56:57 -0600

    changed keymap function to new shorter syntax

--- lua/jake/keymaps.lua
+++ lua/jake/keymaps.lua
@@ -5,7 +5,7 @@ local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
 -- Shorten function name
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
 -- Set leader to space
 keymap("", "<Space>", "<Nop>", opts)

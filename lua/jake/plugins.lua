cad822503cc7177f95062a48a58618f9ecdbca8a
jake jordan <jakesjordan11@gmail.com>
2022-05-07 12:07:39 -0600

    removed telescope-ui-select

--- lua/jake/plugins.lua
+++ lua/jake/plugins.lua
@@ -132,7 +132,6 @@ return packer.startup(function(use)
   use("nvim-telescope/telescope.nvim")
   use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
   use("nvim-telescope/telescope-file-browser.nvim")
-  use({ "nvim-telescope/telescope-ui-select.nvim" })
 
   -- Treesitter
   use({

7ca2c3702ff6f6e1f4d977b5feff586259f5a150
jake jordan <jakesjordan11@gmail.com>
2022-05-07 12:08:25 -0600

    updated keymap to new neovim api

--- lua/jake/keymaps.lua
+++ lua/jake/keymaps.lua
@@ -4,41 +4,34 @@ local opts = { noremap = true, silent = true }
 -- Set terminal options
 local term_opts = { silent = true }
 
--- Shorten function name
-local keymap = vim.keymap.set
-
 -- Set leader to space
-keymap("", "<Space>", "<Nop>", opts)
+vim.keymap.set("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.localleader = " "
 
-keymap("n", "<C-s>", "<cmd>w!<CR>", opts)
+vim.keymap.set("n", "<C-s>", "<cmd>w!<CR>", opts)
 -- use leader w for window mode
 -- keymap("n", "<leader>w", "<C-w>", opts)
 
 -- Navigate buffers keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
 
 -- fix pasting over items yanking them
-keymap("v", "p", '"_dP', opts)
+vim.keymap.set("v", "p", '"_dP', opts)
 
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", opts)
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", opts)
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
--- Rust keymaps
--- keymap("n", "<leader>rf", ":RustFmt<CR>", opts)
--- keymap("n", "<leader>rr", ":RustRun<CR>", opts)
+vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)

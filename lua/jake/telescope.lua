4e599e01b19b12aadb53ef10ca07433a8bc3b3bc
jake jordan <jakesjordan11@gmail.com>
2022-05-07 12:10:34 -0600

    cleaning setup

--- lua/jake/telescope.lua
+++ lua/jake/telescope.lua
@@ -3,44 +3,16 @@ if not status_ok then
   return
 end
 
-local actions = require("telescope.actions")
-
 telescope.setup({
   defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-
-    mappings = {
-      i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-
-        ["<C-c>"] = actions.close,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+  },
+  pickers = {
+    colorscheme = {
+      theme = "dropdown",
+    },
+    find_files = {
+      layout_config = {
+        preview_width = 0.5,
       },
     },
     oldfiles = {
@@ -48,35 +20,16 @@ telescope.setup({
         preview_width = 0.5,
       },
     },
-  },
-  pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+    diagnostics = {
+      theme = "ivy",
+    },
   },
   extensions = {
     file_browser = {
       theme = "ivy",
-      mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
-        },
-        ["n"] = {
-          -- your custom normal mode mappings
-        },
-      },
-    },
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown({
-        -- even more opts
-      }),
     },
   },
 })
 telescope.load_extension("fzf")
 telescope.load_extension("file_browser")
-telescope.load_extension("ui-select")
+telescope.load_extension("projects")

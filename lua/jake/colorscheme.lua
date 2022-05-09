48d16346a9044969e0cedcf978530b2d24924d53
jake jordan <jakesjordan11@gmail.com>
2022-05-09 17:51:16 -0600

    added auto dark and more colors

--- lua/jake/colorscheme.lua
+++ lua/jake/colorscheme.lua
@@ -1,22 +1,71 @@
--- local colorscheme = "none"
--- local colorscheme = "ayu"
--- local colorscheme = "bogster"
--- local colorscheme = "boo"
--- local colorscheme = "catppuccin"
--- local colorscheme = "everforest"
-local colorscheme = "gruvbox"
--- local colorscheme = "halcyon"
--- local colorscheme = "lvim"
--- local colorscheme = "melange"
--- local colorscheme = "night-owl"
--- local colorscheme = "rvcs"
--- local colorscheme = "tokyonight"
--- local colorscheme = "codemonkey"
+local themes = {
+  aurora     = "aurora",
+  ayu        = "ayu",
+  blue       = "blue",
+  bogster    = "bogster",
+  catppuccin = "catppuccin",
+  codemonkey = "codemonkey",
+  darkblue   = "darkblue",
+  darkplus   = "darkplus",
+  default    = "default",
+  delek      = "delek",
+  desert     = "desert",
+  elflord    = "elflord",
+  evening    = "evening",
+  everforest = "everforest",
+  gruvbox    = "gruvbox",
+  halcyon    = "halcyon",
+  industry   = "industry",
+  kanagawa   = "kanagawa",
+  koehler    = "koehler",
+  lunar      = "lunar",
+  lvim       = "lvim",
+  material   = "material",
+  melange    = "melange",
+  morning    = "morning",
+  murphy     = "murphy",
+  night_owl  = "night-owl",
+  nightfly   = "nightfly",
+  none       = "none",
+  neon       = "neon",
+  onedarker  = "onedarker",
+  pablo      = "pablo",
+  peachpuff  = "peachpuff",
+  ron        = "ron",
+  rvcs       = "rvcs",
+  shine      = "shine",
+  slate      = "slate",
+  spacedark  = "spacedark",
+  system76   = "system76",
+  tokyonight = "tokyonight",
+  tomorrow   = "tomorrow",
+  torte      = "torte",
+  zellner    = "zellner",
+}
+
+local colorscheme = themes.gruvbox
+
 vim.api.nvim_set_var(colorscheme .. "_italic_comments", true)
 vim.api.nvim_set_var(colorscheme .. "_italic_keywords", true)
 vim.api.nvim_set_var(colorscheme .. "_italic_functions", true)
 vim.api.nvim_set_var(colorscheme .. "_sidebars", { "qf", "vista_kind", "terminal", "packer" })
--- vim.api.nvim_set_var(colorscheme .. "_style", "night")
+vim.api.nvim_set_var(colorscheme .. "_style", "night")
+
+local auto_dark_mode = require('auto-dark-mode')
+
+auto_dark_mode.setup({
+  update_interval = 500,
+  set_dark_mode = function()
+    vim.opt.background = 'dark'
+    vim.colorscheme = colorscheme
+  end,
+  set_light_mode = function()
+    vim.opt.background = 'light'
+    vim.colorscheme = colorscheme
+  end,
+})
+
+auto_dark_mode.init()
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 

b51aaa5dcbd588343cbb3aefecaa48b23439c070
jake jordan <jakesjordan11@gmail.com>
2022-05-10 16:19:27 -0600

    custom highlights are now used

--- lua/jake/highlights.lua
+++ lua/jake/highlights.lua
@@ -1,17 +1,17 @@
-vim.cmd("highlight SignColumn guibg=NONE") -- background color of signcolumn bar not edits
-vim.cmd("highlight CursorLine guibg=NONE") -- background of current cursor line
-vim.cmd("highlight CursorLineNr guibg=NONE") -- background of number column on current cursor line
-
--- Git Signs remove background highlight in signcolumn
-vim.cmd("highlight GitSignsAdd guibg=NONE")
-vim.cmd("highlight GitSignsChange guibg=NONE")
-vim.cmd("highlight GitSignsDelete guibg=NONE")
-vim.cmd("highlight GitSignsChange guibg=NONE")
-
--- Diagnostics remove background highlight in signcolumn
-vim.cmd("highlight DiagnosticSignError guibg=NONE")
-vim.cmd("highlight DiagnosticSignWarn guibg=NONE")
-vim.cmd("highlight DiagnosticSignInfo guibg=NONE")
-vim.cmd("highlight DiagnosticSignHint guibg=NONE")
-
-vim.cmd("highlight Pmenu ctermbg=NONE guibg=NONE")
+vim.cmd([[
+  augroup jake_highlights
+    autocmd!
+  autocmd ColorScheme * highlight Pmenu ctermbg=NONE guibg=NONE 
+  \ | highlight SignColumn guibg=NONE 
+  \ | highlight CursorLine guibg=NONE 
+  \ | highlight CursorLineNr guibg=NONE 
+  \ | highlight GitSignsAdd guibg=NONE 
+  \ | highlight GitSignsChange guibg=NONE 
+  \ | highlight GitSignsDelete guibg=NONE 
+  \ | highlight DiagnosticSignError guibg=NONE 
+  \ | highlight DiagnosticSignWarn guibg=NONE
+  \ | highlight DiagnosticSignInfo guibg=NONE 
+  \ | highlight DiagnosticSignHint guibg=NONE
+  \ | highlight Pmenu ctermbg=NONE guibg=NONE
+  augroup END
+]])

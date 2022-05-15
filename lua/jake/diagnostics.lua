e040dc8357c65dc361c88dc57c05df5d064838ac
jake jordan <jakesjordan11@gmail.com>
2022-05-15 05:01:27 -0600

    created diagnostic pop up borders

---
+++ lua/jake/diagnostics.lua
@@ -0,0 +1,6 @@
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or 'rounded'
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end

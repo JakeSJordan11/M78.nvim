fa6748d1727f389c2e759f1f8f76d759c159fab7
jake jordan <jakesjordan11@gmail.com>
2022-05-15 05:06:53 -0600

    reformat

--- init.lua
+++ init.lua
@@ -1,25 +1,26 @@
-require("jake.alpha")
-require("jake.autocommands")
-require("jake.autopairs")
-require("jake.bufferline")
-require("jake.circles")
-require("jake.cmp")
-require("jake.colorscheme")
-require("jake.comment")
-require("jake.focus")
-require("jake.gitsigns")
-require("jake.highlights")
-require("jake.indentline")
-require("jake.keymaps")
-require("jake.lsp")
-require("jake.null-ls")
-require("jake.nvim-tree")
-require("jake.options")
-require("jake.plugins")
-require("jake.project")
-require("jake.telescope")
-require("jake.toggleterm")
-require("jake.treesitter")
-require("jake.whichkey")
-require("jake.windline")
-require("jake.zen-mode")
+require "jake.alpha"
+require "jake.autocommands"
+require "jake.autopairs"
+require "jake.bufferline"
+require "jake.circles"
+require "jake.cmp"
+require "jake.colorscheme"
+require "jake.comment"
+require "jake.diagnostics"
+require "jake.focus"
+require "jake.gitsigns"
+require "jake.highlights"
+require "jake.indentline"
+require "jake.keymaps"
+require "jake.lsp"
+require "jake.null-ls"
+require "jake.nvim-tree"
+require "jake.options"
+require "jake.plugins"
+require "jake.project"
+require "jake.telescope"
+require "jake.toggleterm"
+require "jake.treesitter"
+require "jake.whichkey"
+require "jake.windline"
+require "jake.zen-mode"

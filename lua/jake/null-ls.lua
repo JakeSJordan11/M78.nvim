8b26cd95b80cc649100fc15b286a2bd5345c53fd
jake jordan <jakesjordan11@gmail.com>
2022-05-15 21:33:58 -0600

    formatted

--- lua/jake/null-ls.lua
+++ lua/jake/null-ls.lua
@@ -2,52 +2,53 @@ local null_ls = require("null-ls")
 local prettier = require("prettier")
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettierd.with({
-      filetypes = { "css" },
-    }),
-  },
-  on_attach = function(client, _)
-    if client.server_capabilities then
-      vim.cmd("nnoremap <silent><buffer> <Leader>lf :lua vim.lsp.buf.format {async=true}<CR>")
-      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.format {async=true}")
-      vim.cmd("xnoremap <silent><buffer> <Leader>lf :lua vim.lsp.buf.range_formatting({})<CR>")
-    end
-  end,
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettierd.with({
+			filetypes = { "css" },
+		}),
+	},
+	on_attach = function(client, _)
+		if client.server_capabilities then
+			vim.cmd("nnoremap <silent><buffer> <Leader>lf :lua vim.lsp.buf.format {async=true}<CR>")
+			vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.format {async=true}")
+			vim.cmd("xnoremap <silent><buffer> <Leader>lf :lua vim.lsp.buf.range_formatting({})<CR>")
+		end
+	end,
 })
 
 prettier.setup({
-  bin = 'prettierd', -- or `prettierd`
-  filetypes = {
-    "css",
-    "graphql",
-    "html",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "less",
-    "markdown",
-    "scss",
-    "typescript",
-    "typescriptreact",
-    "yaml",
-  },
+	bin = "prettierd", -- or `prettierd`
+	filetypes = {
+		"css",
+		"graphql",
+		"html",
+		"javascript",
+		"javascriptreact",
+		"json",
+		"less",
+		"markdown",
+		"scss",
+		"typescript",
+		"typescriptreact",
+		"yaml",
+	},
 
-  -- prettier format options (you can use config files too. ex: `.prettierrc`)
-  arrow_parens = "always",
-  bracket_spacing = true,
-  embedded_language_formatting = "auto",
-  end_of_line = "lf",
-  html_whitespace_sensitivity = "css",
-  jsx_bracket_same_line = false,
-  jsx_single_quote = false,
-  print_width = 80,
-  prose_wrap = "preserve",
-  quote_props = "as-needed",
-  semi = true,
-  single_quote = false,
-  tab_width = 2,
-  trailing_comma = "es5",
-  use_tabs = false,
-  vue_indent_script_and_style = false,
+	-- prettier format options (you can use config files too. ex: `.prettierrc`)
+	arrow_parens = "always",
+	bracket_spacing = true,
+	embedded_language_formatting = "auto",
+	end_of_line = "lf",
+	html_whitespace_sensitivity = "css",
+	jsx_bracket_same_line = false,
+	jsx_single_quote = false,
+	print_width = 80,
+	prose_wrap = "preserve",
+	quote_props = "as-needed",
+	semi = true,
+	single_quote = false,
+	tab_width = 2,
+	trailing_comma = "es5",
+	use_tabs = false,
+	vue_indent_script_and_style = false,
 })

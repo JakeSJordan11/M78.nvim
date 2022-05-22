7a2bb0fbe43f94d9d037680c019f36b149ce02b9
jake jordan <jakesjordan11@gmail.com>
2022-05-22 10:16:24 -0600

    tranfered highlights to lua

--- lua/jake/highlights.lua
+++ lua/jake/highlights.lua
@@ -1,24 +1,20 @@
 vim.cmd([[
-  augroup jake_highlights
-    autocmd!
-  autocmd ColorScheme * highlight Pmenu ctermbg=NONE guibg=NONE 
-  \ | highlight SignColumn guibg=NONE 
-  \ | highlight CursorLine guibg=NONE 
-  \ | highlight CursorLineNr guibg=NONE 
-  \ | highlight GitSignsAdd guibg=NONE 
-  \ | highlight GitSignsChange guibg=NONE 
-  \ | highlight GitSignsDelete guibg=NONE 
-  \ | highlight DiagnosticSignError guibg=NONE
-  \ | highlight DiagnosticSignWarn guibg=NONE
-  \ | highlight DiagnosticSignInfo guibg=NONE
-  \ | highlight DiagnosticSignHint guibg=NONE
-  \ | highlight NormalFloat guibg=NONE
-  augroup END
-]])
-
-vim.cmd([[
   augroup yank_highlight
     autocmd!
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'IncSearch', timeout = 200})
   augroup END
 ]])
+
+vim.api.nvim_set_hl(0, 'CursorLine', {})
+vim.api.nvim_set_hl(0, 'CursorLineNr', {})
+vim.api.nvim_set_hl(0, 'DiagnosticSignError', { fg = '#fb4934' })
+vim.api.nvim_set_hl(0, 'DiagnosticSignWarn', { fg = '#fabd2f' })
+vim.api.nvim_set_hl(0, 'DiagnosticSignHint', { fg = '#8ec07c' })
+vim.api.nvim_set_hl(0, 'DiagnosticSignInfo', { fg = '#83a598' })
+vim.api.nvim_set_hl(0, 'GitSignsAdd', {})
+vim.api.nvim_set_hl(0, 'GitSignsChange', {})
+vim.api.nvim_set_hl(0, 'GitSignsDelete', {})
+vim.api.nvim_set_hl(0, 'NormalFloat', {})
+vim.api.nvim_set_hl(0, 'Pmenu', {})
+vim.api.nvim_set_hl(0, 'SignColumn', {})
+vim.api.nvim_set_hl(0, 'WildMenu', {})

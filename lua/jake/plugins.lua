1745ff20a1b8193269c546d28c74844a52b0fd0d
Jake Jordan <JakeSJordan11@Gmail.com>
2022-07-31 02:02:13 -0600

    removed unused plugins and references

--- lua/jake/plugins.lua
+++ lua/jake/plugins.lua
@@ -44,35 +44,19 @@ return packer.startup(function(use)
   -- AI assistance
   use "github/copilot.vim"
 
-  -- AutoPairs
-  -- use "windwp/nvim-autopairs"
-
   -- Bufferline
   use "akinsho/bufferline.nvim"
 
   -- Colorschemes
-  use "themercorp/themer.lua"
-  use "EdenEast/nightfox.nvim"
-  use "Mofiqul/adwaita.nvim"
   use "jadnw/nightwing.nvim"
-  use "jadnw/gemstones.nvim"
-  use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
+  use "ellisonleao/gruvbox.nvim"
+  use "rockerBOO/boo-colorscheme-nvim"
   use "catppuccin/nvim"
   use "savq/melange"
   use "Julpikar/night-owl.nvim"
-  use "CantoroMC/ayu-nvim"
-  use "ellisonleao/gruvbox.nvim"
-  use "rebelot/kanagawa.nvim"
-  use "rockerBOO/boo-colorscheme-nvim"
-  use "kwsp/halcyon-neovim"
   use "eddyekofo94/bogster.nvim"
   use "folke/tokyonight.nvim"
-  use "marko-cerovac/material.nvim"
   use "sainnhe/everforest"
-  use "shaeinst/roshnivim"
-  use "shaeinst/roshnivim-cs"
-  use "lvim-tech/lvim-colorscheme"
 
   -- Comments
   use "numToStr/Comment.nvim"
@@ -90,15 +74,11 @@ return packer.startup(function(use)
   -- Diagnostics
   use "folke/trouble.nvim"
 
-  -- Debugging
-  -- use "mfussenegger/nvim-dap"
-
   -- Git
   use "lewis6991/gitsigns.nvim"
 
   -- Icons
   use "kyazdani42/nvim-web-devicons"
-  use "projekt0n/circles.nvim"
 
   -- Keymaps
   use "folke/which-key.nvim"
@@ -107,88 +87,42 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer"
   use "neovim/nvim-lspconfig"
   use "jose-elias-alvarez/null-ls.nvim"
-  -- use "MunifTanjim/prettier.nvim"
 
-  -- Notifications
-  -- use "rcarriga/nvim-notify"
-
-  -- MiniMap
-  -- use "wfxr/minimap.vim"
-
-  -- Lua
-  -- use "ckipp01/stylua-nvim"
+  -- Navigation
+  use "kyazdani42/nvim-tree.lua"
+  use "ThePrimeagen/harpoon"
+  use "ahmedkhalf/project.nvim"
+  use "goolord/alpha-nvim"
+  use "nvim-telescope/telescope-file-browser.nvim"
+  use "nvim-telescope/telescope.nvim"
+  use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+  use "nvim-telescope/telescope-ui-select.nvim"
 
   -- Project Management
   use "wakatime/vim-wakatime"
-  use "ahmedkhalf/project.nvim"
-  use "goolord/alpha-nvim"
-  -- use { "kyazdani42/nvim-tree.lua", tag = "nightly" }
-  use "kyazdani42/nvim-tree.lua"
 
   -- Rust
   use "simrat39/rust-tools.nvim"
   use "rust-lang/rust.vim"
-  use {
-    "saecki/crates.nvim",
-    tag = "v0.2.1",
-    -- requires = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("crates").setup()
-    end,
-  }
+  use "saecki/crates.nvim"
 
   -- Statusline
   use "windwp/windline.nvim"
-  -- use "nvim-lualine/lualine.nvim"
 
   -- Snippets
-  -- use "rafamadriz/friendly-snippets"
   use "L3MON4D3/LuaSnip"
 
-  -- Tabline
-  -- use "nanozuki/tabby.nvim"
-
-  -- Telescope
-  use "nvim-telescope/telescope.nvim"
-  use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
-  use "nvim-telescope/telescope-file-browser.nvim"
-  use { "nvim-telescope/telescope-ui-select.nvim" }
-  use {
-    "nvim-telescope/telescope-frecency.nvim",
-    requires = { "tami5/sqlite.lua" },
-  }
-
   -- Terminal
-  use "https://git.sr.ht/~havi/telescope-toggleterm.nvim"
-  use { "akinsho/toggleterm.nvim", tag = "v2.*" }
+  use "akinsho/toggleterm.nvim"
 
   -- Treesitter
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  }
-  -- use "nvim-treesitter/playground"
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
   use "JoosepAlviste/nvim-ts-context-commentstring"
-  -- use "windwp/nvim-ts-autotag"
-  -- use "nvim-treesitter/nvim-treesitter-textobjects"
-  -- use "nvim-treesitter/nvim-treesitter-refactor"
-  -- use "nvim-treesitter/nvim-treesitter-context"
 
-  -- UI
-  -- use "MunifTanjim/nui.nvim"
+  -- Window Management
   use "beauwilliams/focus.nvim"
-  use "folke/zen-mode.nvim"
-  -- use {
-  -- "folke/twilight.nvim",
-  -- config = function()
-  -- require("twilight").setup {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
-  -- }
-  -- end,
-  -- }
-  -- use "lukas-reineke/indent-blankline.nvim"
+
+  -- Dependencies
   use "nvim-lua/popup.nvim"
   use "nvim-lua/plenary.nvim"
 

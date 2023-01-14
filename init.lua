1cdd94e7213727434a0bfa90e7ca8cc0e67c2763
jake jordan <jakesjordan11@gmail.com>
2023-01-14 16:53:07 -0700

    Waay too much spice

--- init.lua
+++ init.lua
@@ -1,40 +1,11 @@
-require "jake.alpha"
-require "jake.cmp"
-require "jake.colorsheme"
-require "jake.comment"
-require "jake.diagnostics"
-require "jake.fidget"
-require "jake.gitsigns"
-require "jake.gui-font-resize"
-require "jake.highlights"
-require "jake.highlight-on-yank"
-require "jake.keymaps"
-require "jake.lsp"
-require "jake.nvim-tree"
-require "jake.neodev"
-require "jake.neovide"
-require "jake.options"
-require "jake.plugins"
-require "jake.project"
-require "jake.telescope"
-require "jake.toggleterm"
-require "jake.treesitter"
-require "jake.whichkey"
-require "jake.windline"
-local ui = {
-  icons = {
-    cmd = "⌘",
-    config = "🛠",
-    event = "📅",
-    ft = "📂",
-    init = "⚙",
-    keys = "🗝",
-    plugin = "🔌",
-    runtime = "💻",
-    source = "📄",
-    start = "🚀",
-    task = "📌",
-  },
-}
+require "config.options"
+require "config.lazy"
+require "config.neovide"
 
-return ui
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require "config.autocmds"
+    require "config.keymaps"
+  end,
+})

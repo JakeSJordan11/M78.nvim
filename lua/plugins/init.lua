ee3fa252158abb58bd659abb3e71f7f032bed195
jake jordan <jakesjordan11@gmail.com>
2023-01-15 22:58:43 -0700

    [refactor] - remove unused plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,26 +1,13 @@
 return {
   'nvim-lua/plenary.nvim',
-  -- 'MunifTanjim/nui.nvim',
   {
     'nvim-tree/nvim-web-devicons',
     config = { default = true },
   },
-  { 'nacro90/numb.nvim', event = 'BufReadPre', config = true },
-  --[[ {
-    'lukas-reineke/indent-blankline.nvim',
-    event = 'BufReadPre',
-    config = true,
-  }, ]]
   {
     'stevearc/dressing.nvim',
     event = 'VeryLazy',
     config = true,
-  },
-  {
-    'rcarriga/nvim-notify',
-    event = 'VeryLazy',
-    enabled = true,
-    config = { default = true }, -- same as config = true
   },
   {
     'sindrets/diffview.nvim',

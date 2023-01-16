d09ce09f8c43d541cdfc24baaca74eccd97c59a3
jake jordan <jakesjordan11@gmail.com>
2023-01-15 22:36:53 -0700

    [refactor] - move colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,11 +3,6 @@ return {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
-    --[[ config = function()
-      local tokyonight = require 'tokyonight'
-      tokyonight.setup { style = 'storm' }
-      tokyonight.load()
-    end, ]]
   },
   {
     'catppuccin/nvim',

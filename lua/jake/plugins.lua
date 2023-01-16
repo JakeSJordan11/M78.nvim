1a448c87a4b70253edd0eb8e4da0a0316694b5c2
jake jordan <jakesjordan11@gmail.com>
2023-01-16 14:36:35 -0700

    telescope updates

--- lua/jake/plugins.lua
+++ lua/jake/plugins.lua
@@ -151,6 +151,7 @@ return packer.startup(function(use)
 
   -- Telescope
   use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-ui-select.nvim'
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use 'nvim-telescope/telescope-file-browser.nvim'
 
--- lua/jake/telescope.lua
+++ lua/jake/telescope.lua
@@ -1,20 +1,22 @@
 require('telescope').setup {
+  defaults = {
+    layout_config = {
+      prompt_position = 'top',
+    },
+    sorting_strategy = 'ascending',
+  },
   pickers = {
     colorscheme = {
       theme = 'dropdown',
     },
-    commands = {
-      theme = 'ivy',
-    },
-    keymaps = {
-      theme = 'ivy',
-    },
   },
   extensions = {
     file_browser = {
-      theme = 'ivy',
+      previewer = false,
     },
   },
 }
-require('telescope').load_extension 'file_browser'
+pcall(require('telescope').load_extension, 'fzf')
 require('telescope').load_extension 'projects'
+require('telescope').load_extension 'ui-select'
+require('telescope').load_extension 'file_browser'

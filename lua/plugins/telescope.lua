4dc10e0a0ff326b6602a8120b9205f54cf3a5139
jake jordan <jakesjordan11@gmail.com>
2023-01-15 23:03:39 -0700

    [refactor] - codesplit init plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
+    'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-file-browser.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
     'nvim-telescope/telescope-fzf-native.nvim',

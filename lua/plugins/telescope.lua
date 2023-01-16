660a720bf3779a178a473cd9ef4b5699b25ef06b
jake jordan <jakesjordan11@gmail.com>
2023-01-15 23:29:40 -0700

    [refactor] - remove unused telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,14 +7,6 @@ return {
     'nvim-telescope/telescope-fzf-native.nvim',
   },
   cmd = 'Telescope',
-  keys = {
-    { '<leader><space>', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
-    { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
-    { '<leader>fr', '<cmd>Telescope oldfiles<cr>', desc = 'Recent' },
-    { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
-    { '<leader>fg', '<cmd>Telescope git_files<cr>', desc = 'Git Files' },
-    { '<leader>f/', '<cmd>Telescope live_grep<cr>', desc = 'Grep' },
-  },
   opts = function()
     require('telescope').setup {
       defaults = {

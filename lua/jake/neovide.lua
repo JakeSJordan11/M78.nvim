23d70428a3be168ed35189282f0b1e7858e4f4d8
jake jordan <jakesjordan11@gmail.com>
2023-01-16 19:25:01 -0700

    updated font size

--- lua/jake/neovide.lua
+++ lua/jake/neovide.lua
@@ -5,4 +5,4 @@ vim.api.nvim_set_var('neovide_cursor_vfx_particle_speed', 10.0)
 vim.api.nvim_set_var('neovide_input_use_logo', true)
 vim.api.nvim_set_var('neovide_macos_alt_is_meta', false)
 vim.api.nvim_set_var('neovide_hide_mouse_when_typing', true)
-vim.opt.guifont = 'OperatorMonoSSmLig NF:h18'
+vim.opt.guifont = 'OperatorMonoSSmLig NF:h16'

1189bd75e2656f08b8d90d07e83573a98adacb7a
jake jordan <jakesjordan11@gmail.com>
2023-01-16 21:57:58 -0700

    [refactor] - remove local variable

--- lua/jake/keymaps.lua
+++ lua/jake/keymaps.lua
@@ -1,42 +1,40 @@
-local keymap = vim.keymap.set
-
 -- Remap for dealing with word wrap
-keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
-keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 -- Better viewing
-keymap('n', 'n', 'nzzzv')
-keymap('n', 'N', 'Nzzzv')
-keymap('n', 'g,', 'g,zvzz')
-keymap('n', 'g;', 'g;zvzz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', 'g,', 'g,zvzz')
+vim.keymap.set('n', 'g;', 'g;zvzz')
 
 -- Better indent
-keymap('v', '<', '<gv')
-keymap('v', '>', '>gv')
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 
 -- Paste over currently selected text without yanking it
-keymap('v', 'p', '"_dP')
+vim.keymap.set('v', 'p', '"_dP')
 
 -- Move Lines
-keymap('n', '<A-j>', ':m .+1<CR>==')
-keymap('v', '<A-j>', ":m '>+1<CR>gv=gv")
-keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi')
-keymap('n', '<A-k>', ':m .-2<CR>==')
-keymap('v', '<A-k>', ":m '<-2<CR>gv=gv")
-keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi')
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==')
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv")
+vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi')
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==')
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv")
+vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi')
 
 -- Resize window using <shift> arrow keys
-keymap('n', '<S-Up>', '<cmd>resize +2<CR>')
-keymap('n', '<S-Down>', '<cmd>resize -2<CR>')
-keymap('n', '<S-Left>', '<cmd>vertical resize -2<CR>')
-keymap('n', '<S-Right>', '<cmd>vertical resize +2<CR>')
+vim.keymap.set('n', '<S-Up>', '<cmd>resize +2<CR>')
+vim.keymap.set('n', '<S-Down>', '<cmd>resize -2<CR>')
+vim.keymap.set('n', '<S-Left>', '<cmd>vertical resize -2<CR>')
+vim.keymap.set('n', '<S-Right>', '<cmd>vertical resize +2<CR>')
 
 -- control s save
-keymap('n', '<C-s>', '<cmd>w!<CR>')
+vim.keymap.set('n', '<C-s>', '<cmd>w!<CR>')
 
 -- switch buffers
-keymap('n', '<S-l>', ':bnext<CR>', { silent = true })
-keymap('n', '<S-h>', ':bprevious<CR>', { silent = true })
+vim.keymap.set('n', '<S-l>', ':bnext<CR>', { silent = true })
+vim.keymap.set('n', '<S-h>', ':bprevious<CR>', { silent = true })
 
-keymap('n', '/', '<cmd>SearchBoxIncSearch<CR>', { noremap = true })
-keymap('n', ':', '<cmd>FineCmdline<CR>', { noremap = true })
+vim.keymap.set('n', '/', '<cmd>SearchBoxIncSearch<CR>', { noremap = true })
+vim.keymap.set('n', ':', '<cmd>FineCmdline<CR>', { noremap = true })

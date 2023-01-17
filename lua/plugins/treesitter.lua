f77e053d03eecdc2ad23142fc29762143c82f21a
jake jordan <jakesjordan11@gmail.com>
2023-01-17 15:45:55 -0700

    [refactor] - removing helper code

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,96 +1,20 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', 'JoosepAlviste/nvim-ts-context-commentstring',
-      'windwp/nvim-ts-autotag', 'nvim-treesitter/playground' },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      'windwp/nvim-ts-autotag',
+      'nvim-treesitter/playground',
+      'nvim-treesitter/nvim-treesitter-context',
+    },
     build = ':TSUpdate',
     event = 'BufReadPost',
     config = function()
-      local swap_next, swap_prev = (function()
-        local swap_objects = {
-          p = '@parameter.inner',
-          f = '@function.outer',
-          c = '@class.outer',
-        }
-
-        local n, p = {}, {}
-        for key, obj in pairs(swap_objects) do
-          n[string.format('<Leader>cx%s', key)] = obj
-          p[string.format('<Leader>cX%s', key)] = obj
-        end
-
-        return n, p
-      end)()
-
       require('nvim-treesitter.configs').setup {
-        ensure_installed = {
-          'bash',
-          'help',
-          'html',
-          'javascript',
-          'json',
-          'lua',
-          'markdown',
-          'markdown_inline',
-          'python',
-          'query',
-          'regex',
-          'rust',
-          'tsx',
-          'typescript',
-          'vim',
-          'yaml',
-        },
-        highlight = { enable = true },
-        indent = { enable = true, disable = { 'python' } },
-        incremental_selection = {
+        highlight = {
           enable = true,
-          keymaps = {
-            init_selection = 'gnn',
-            node_incremental = 'grn',
-            scope_incremental = 'grc',
-            node_decremental = 'grm',
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.outer',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.outer',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = swap_next,
-            swap_previous = swap_prev,
-          },
+          additional_vim_regex_highlighting = false,
         },
       }
     end,

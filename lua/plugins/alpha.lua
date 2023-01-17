e6b8ead295f78bf6f6a938888df29cf9470963af
jake jordan <jakesjordan11@gmail.com>
2023-01-17 15:49:57 -0700

    format

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -5,38 +5,38 @@ return {
     local dashboard = require 'alpha.themes.dashboard'
     dashboard.section.header.val = {
       "         You'll Find it Bud, I Promise    ",
-      "",
-      "    ooooooooooooooooooooooooooooooooooooo",
+      '',
+      '    ooooooooooooooooooooooooooooooooooooo',
       "    8                                .d88',",
       "    8   oooooooooooooooooooooooooooo 8888',",
-      "    8  8888888888888888888888888P\"   8888',",
-      "    8  8888888888888888888888P\"      8888',",
-      "    8  8888888888888888888P\"         8888',",
-      "    8  8888888888888888P\"            8888',",
-      "    8  8888888888888P\"               8888',",
-      "    8  8888888888P\"                  8888',",
-      "    8  8888888P\"                     8888',",
-      "    8  8888P\"                        8888',",
+      '    8  8888888888888888888888888P"   8888\',',
+      '    8  8888888888888888888888P"      8888\',',
+      '    8  8888888888888888888P"         8888\',',
+      '    8  8888888888888888P"            8888\',',
+      '    8  8888888888888P"               8888\',',
+      '    8  8888888888P"                  8888\',',
+      '    8  8888888P"                     8888\',',
+      '    8  8888P"                        8888\',',
       "    8 d8888888888888888888888888888888888',",
-      "    8 88888888888888888888888888888888888/",
+      '    8 88888888888888888888888888888888888/',
       "           8ooooooooooooooocgmm8888\\'",
-      "       ooooooooooooooooooooooooooooooo,",
+      '       ooooooooooooooooooooooooooooooo,',
       "      d                       ...oood8b',",
       "     d              ...oood888888888888b',",
       "    d     ...oood88888888888888888888888b',",
       "   dood8888888888888888888888888888888888b'",
     }
     dashboard.section.buttons.val = {
-      dashboard.button("p", "  Find project", "<Cmd>Telescope projects<CR>"),
-      dashboard.button("r", "  Recently used files", "<Cmd>Telescope oldfiles<CR>"),
-      dashboard.button("c", "  Configuration", "<Cmd>e ~/.config/nvim/init.lua<CR>"),
-      dashboard.button("q", "  Quit Neovim", "<Cmd>qa<CR>"),
+      dashboard.button('p', '  Find project', '<Cmd>Telescope projects<CR>'),
+      dashboard.button('r', '  Recently used files', '<Cmd>Telescope oldfiles<CR>'),
+      dashboard.button('c', '  Configuration', '<Cmd>e ~/.config/nvim/init.lua<CR>'),
+      dashboard.button('q', '  Quit Neovim', '<Cmd>qa<CR>'),
     }
 
     for _, value in ipairs(dashboard.section.buttons.val) do
-      value.opts.hl = "Constant"
+      value.opts.hl = 'Constant'
     end
     dashboard.opts.opts.noautocmd = true
-    require 'alpha'.setup(dashboard.opts)
+    require('alpha').setup(dashboard.opts)
   end,
 }

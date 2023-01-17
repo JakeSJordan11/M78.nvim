26a077d7bba2c0dafd214ed8a3bc338f9dae23a9
jake jordan <jakesjordan11@gmail.com>
2023-01-16 22:27:39 -0700

    [feature] setup trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,3 +1,7 @@
 return {
-  'folke/trouble.nvim',
+  { 'folke/trouble.nvim',
+    opts = function()
+      vim.keymap.set({ 'n', 'v' }, '<leader>ld', '<cmd>TroubleToggle<cr>', { silent = true })
+    end
+  }
 }

6dae40d72b5f901fdbb24ea1660f406fb33308fe
jake jordan <jakesjordan11@gmail.com>
2023-01-19 06:03:07 -0700

    [refactor] - trouble setup

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -13,7 +13,7 @@ function M.on_attach()
 
   vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
 
-  local diagnostic_signs = { Error = 'E', Warn = 'W', Hint = 'H', Info = 'I' }
+  local diagnostic_signs = { Error = '', Warn = '', Hint = '', Info = '', Other = '﫠' }
   for type, icon in pairs(diagnostic_signs) do
     local hl = 'DiagnosticSign' .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,7 +1,3 @@
 return {
-  { 'folke/trouble.nvim',
-    opts = function()
-      vim.keymap.set({ 'n', 'v' }, '<Leader>ld', '<Cmd>TroubleToggle<CR>', { silent = true })
-    end
-  }
+  { 'folke/trouble.nvim', config = true },
 }

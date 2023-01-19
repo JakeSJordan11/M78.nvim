14f24dbb8f483a3e4f252bc29f0183d1755b8d1b
jake jordan <jakesjordan11@gmail.com>
2023-01-19 15:40:10 -0700

    updated lazy ui

--- lua/jake/lazy.lua
+++ lua/jake/lazy.lua
@@ -10,4 +10,14 @@ if not vim.loop.fs_stat(lazypath) then
   }
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup 'plugins'
+require('lazy').setup {
+  spec = {
+    import = 'plugins',
+  },
+  install = {
+    colorscheme = { 'gruvbox' },
+  },
+  ui = {
+    border = 'rounded',
+  },
+}

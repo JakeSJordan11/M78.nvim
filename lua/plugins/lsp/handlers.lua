d3da03782d6782e78e98887f72bd85fdf4db7525
jake jordan <jakesjordan11@gmail.com>
2023-01-21 16:21:16 -0700

    changed diagnotic signs

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.on_attach()
-  local diagnostic_signs = { Error = '', Warn = '', Hint = '', Info = '', Other = '﫠' }
+  local diagnostic_signs = { Error = '', Warn = '', Hint = '', Info = '', Other = '' }
   for type, icon in pairs(diagnostic_signs) do
     local hl = 'DiagnosticSign' .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })

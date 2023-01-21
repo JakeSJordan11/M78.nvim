8ba77ac5dc32e3cba8123ff5c6e7325e842508ed
jake jordan <jakesjordan11@gmail.com>
2023-01-21 15:28:14 -0700

    remove unneeded keymap

--- lua/jake/keymaps.lua
+++ lua/jake/keymaps.lua
@@ -1,4 +1,4 @@
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })

1aac044086b3a28c236f4d0fc3aec5d5fb104ae0
jake jordan <jakesjordan11@gmail.com>
2023-01-22 13:36:09 -0700

    changed keymaps

--- lua/plugins/lsp/keymaps.lua
+++ lua/plugins/lsp/keymaps.lua
@@ -1,27 +1,28 @@
 local M = {}
 M.on_attach = function(_, bufnr)
   local wk = require 'which-key'
+  local lsp_format = require 'plugins.lsp.format'
   wk.register {
+    ['K'] = { vim.lsp.buf.hover, 'Hover', buffer = bufnr },
+    ['<C-k>'] = { vim.lsp.buf.signature_help, 'Signature Help', buffer = bufnr },
     ['<Leader>l'] = {
       name = 'LSP',
       a = { vim.lsp.buf.code_action, 'Code Action', buffer = bufnr },
       D = { vim.lsp.buf.type_definition, 'Type Definition', buffer = bufnr },
       d = { '<Cmd>TroubleToggle<CR>', 'Toggle Trouble' },
-      f = { require('plugins.lsp.format').format, 'Format', buffer = bufnr },
-      h = { vim.lsp.buf.hover, 'Hover', buffer = bufnr },
+      f = { lsp_format.format, 'Format', buffer = bufnr },
       j = { vim.diagnostic.goto_next, 'Next Diagnostic', buffer = bufnr },
       k = { vim.diagnostic.goto_prev, 'Previous Diagnostic', buffer = bufnr },
       l = { vim.lsp.codelens.run, 'CodeLens Action' },
       r = { vim.lsp.buf.rename, 'Rename', buffer = bufnr },
       i = { '<Cmd>LspInfo<CR>', 'Lsp Info', buffer = bufnr },
-      s = { vim.lsp.buf.signature_help, 'Signature Help', buffer = bufnr },
     },
     ['g'] = {
       name = 'Goto',
-      i = { '<Cmd>lua vim.lsp.buf.implementation()<CR>', 'Implementation', buffer = bufnr },
-      D = { '<Cmd>lua vim.lsp.buf.declaration()<CR>', 'Declaration', buffer = bufnr },
-      d = { '<Cmd>lua vim.lsp.buf.definition()<CR>', 'Definition', buffer = bufnr },
-      r = { '<Cmd>lua vim.lsp.buf.references()<CR>', 'References', buffer = bufnr },
+      i = { vim.lsp.buf.implementation, 'Implementation', buffer = bufnr },
+      D = { vim.lsp.buf.declaration, 'Declaration', buffer = bufnr },
+      d = { vim.lsp.buf.definition, 'Definition', buffer = bufnr },
+      r = { vim.lsp.buf.references, 'References', buffer = bufnr },
     },
   }
 end

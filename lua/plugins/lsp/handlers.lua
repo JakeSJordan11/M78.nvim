c98a93fe2d74d43feebdac904d8c8d234a546c6b
jake jordan <jakesjordan11@gmail.com>
2023-01-22 20:57:52 -0700

    remove diagnostic signs and clean up

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -1,17 +1,11 @@
 local M = {}
 
 function M.on_attach()
-  vim.diagnostic.config { virtual_text = false, float = { border = 'rounded' } }
-  local diagnostic_signs = {
-    Error = '',
-    Warn = '',
-    Hint = '',
-    Info = '',
-  }
-  for type, icon in pairs(diagnostic_signs) do
-    local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-  end
+  vim.diagnostic.config { float = { border = 'rounded' } }
+  vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    signs = false,
+    virtual_text = false,
+  })
 end
 
 return M

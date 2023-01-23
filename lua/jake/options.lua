c89dc1d9b78bf469b2f89be05e0eac6367783744
jake jordan <jakesjordan11@gmail.com>
2023-01-23 02:12:52 -0700

    joined old and new options

--- lua/jake/options.lua
+++ lua/jake/options.lua
@@ -1,22 +1,18 @@
+vim.opt.highlight.blend = 100
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes'
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.updatetime = 1000
-vim.opt.fillchars = { eob = ' ' }
 vim.opt.showmode = false
 vim.opt.ignorecase = true
+vim.opt.incsearch = true
 vim.opt.smartcase = true
 vim.opt.termguicolors = true
-vim.opt.completeopt = 'menuone,noselect'
 vim.opt.autoindent = true
 vim.opt.cindent = true
 vim.opt.wrap = false
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.belloff = 'all'
 vim.opt.hidden = true
 vim.opt.joinspaces = false
 vim.opt.undofile = true
@@ -26,7 +22,38 @@ vim.opt.timeout = true
 vim.opt.timeoutlen = 300
 vim.opt.laststatus = 3
 vim.opt.splitkeep = 'screen'
-vim.opt.pumheight = 10
+vim.opt.formatoptions:append 'j'
+vim.opt.autoread = true
+vim.opt.mouse = 'a'
+vim.opt.syntax = 'on'
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 5
+vim.opt.linebreak = true
+vim.opt.display:append 'lastline'
+vim.opt.complete:remove 'i'
+vim.opt.shell = '/bin/zsh'
+vim.opt.concealcursor = 'i'
+vim.opt.errorbells = false
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 2
+vim.opt.smarttab = true
+vim.opt.tabstop = 2
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.wildmenu = true
+vim.opt.wildignore:append '.pyc,.swp'
+vim.opt.visualbell = true
+vim.opt.title = true
+vim.opt.confirm = false
+vim.opt.foldmethod = 'indent'
+vim.opt.foldnestmax = 3
+vim.opt.foldenable = false
+vim.opt.backup = false
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.nrformats:remove 'octal'
+vim.opt.history = 1000
+vim.opt.modeline = false
+vim.opt.spell = false
+vim.opt.fillchars = 'eob: '
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '

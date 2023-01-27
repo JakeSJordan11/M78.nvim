e3d57a9829507738d29ceac0a7ed054c0d03bcbf
jake jordan <jakesjordan11@gmail.com>
2023-01-27 02:20:28 -0700

    🔧 fix: noice lsp warnings

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -19,6 +19,13 @@ return {
           command_palette = true,
           lsp_doc_border = true,
         },
+        lsp = {
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true,
+          },
+        },
       }
     end,
   },

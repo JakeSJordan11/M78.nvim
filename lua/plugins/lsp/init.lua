beab84ec43ded001b132e9cabfbf4b19df56af53
jake jordan <jakesjordan11@gmail.com>
2023-02-06 04:31:17 -0700

    🚚 moved mason config to lsp dependency

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -4,9 +4,39 @@ return {
     event = 'BufReadPre',
     dependencies = {
       { 'folke/neodev.nvim', config = true },
+      {
+        'williamboman/mason.nvim',
+        dependencies = {
+          'williamboman/mason-lspconfig.nvim',
+        },
+        config = function()
+          require('mason').setup {
+            ui = {
+              border = 'rounded',
+              height = 0.8,
+            },
+          }
+
+          local ensure_installed = {
+            'stylua',
+            -- 'rustfmt',
+            'prettierd',
+            'typescript-language-server',
+          }
+          local mr = require 'mason-registry'
+          for _, tool in ipairs(ensure_installed) do
+            local p = mr.get_package(tool)
+            if not p:is_installed() then
+              p:install()
+            end
+          end
+          local wk = require 'which-key'
+          wk.register {
+            ['<Leader>m'] = { '<Cmd>Mason<CR>', 'Mason' },
+          }
+        end,
+      },
       'simrat39/rust-tools.nvim',
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-nvim-lsp-signature-help',
       'jose-elias-alvarez/typescript.nvim',

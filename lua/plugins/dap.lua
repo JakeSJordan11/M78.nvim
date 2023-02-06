b216d22b4f306dac530997d07b218d9190d2b65b
Jake Jordan <jake@Jakes-MacBook-Pro.local>
2023-02-05 20:39:11 -0700

    🚧 dap config updates

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,12 +2,15 @@ return {
   {
     'mfussenegger/nvim-dap',
     dependencies = {
-      'jbyuki/one-small-step-for-vimkind',
       'jayp0521/mason-nvim-dap.nvim',
       'theHamsta/nvim-dap-virtual-text',
       'rcarriga/nvim-dap-ui',
       'williamboman/mason.nvim',
       'jayp0521/mason-nvim-dap.nvim',
+      'nvim-telescope/telescope-dap.nvim',
+      'jbyuki/one-small-step-for-vimkind',
+      'mxsdev/nvim-dap-vscode-js',
+      'microsoft/vscode-js-debug',
     },
     config = function()
       local wk = require 'which-key'
@@ -26,16 +29,26 @@ return {
         },
       }
 
-      dap.configurations.lua = {
-        {
-          type = 'nlua',
-          request = 'attach',
-          name = 'Attach to running Neovim instance',
-        },
-      }
-
-      dap.adapters.nlua = function(callback, config)
-        callback { type = 'server', host = config.host or '127.0.0.1', port = config.port or 8086 }
+      for _, language in ipairs { 'typescriptreact', 'javascriptreact' } do
+        require('dap').configurations[language] = {
+          {
+            type = 'pwa-chrome',
+            name = 'Attach - Remote Debugging',
+            request = 'attach',
+            program = '${file}',
+            cwd = vim.fn.getcwd(),
+            sourceMaps = true,
+            protocol = 'inspector',
+            port = 9222,
+            webRoot = '${workspaceFolder}',
+          },
+          {
+            type = 'pwa-chrome',
+            name = 'Launch Chrome',
+            request = 'launch',
+            url = 'http://localhost:3000',
+          },
+        }
       end
     end,
   },

1aacc4353f4e313f110783fa4995db365d809731
jake jordan <jakesjordan11@gmail.com>
2023-02-13 14:58:08 -0700

    💄 moved notify highlights to gruvbox to work with light and dark switching

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -16,6 +16,15 @@ return {
         GruvboxBlueSign = { bg = 'NONE' },
         GruvboxPurpleSign = { bg = 'NONE' },
         GruvboxYellowSign = { bg = 'NONE' },
+        NotifyERRORBorder = { link = 'DiagnosticError' },
+        NotifyERRORIcon = { link = 'DiagnosticError' },
+        NotifyERRORTitle = { link = 'DiagnosticError' },
+        NotifyWARNBorder = { link = 'DiagnosticWarn' },
+        NotifyWARNIcon = { link = 'DiagnosticWarn' },
+        NotifyWARNTitle = { link = 'DiagnosticWarn' },
+        NotifyINFOBorder = { link = 'DiagnosticInfo' },
+        NotifyINFOIcon = { link = 'DiagnosticInfo' },
+        NotifyINFOTitle = { link = 'DiagnosticInfo' },
       },
     }
     gruvbox.load()
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -6,15 +6,6 @@ return {
       {
         'rcarriga/nvim-notify',
         config = function()
-          vim.api.nvim_set_hl(0, 'NotifyERRORBorder', { link = 'DiagnosticError' })
-          vim.api.nvim_set_hl(0, 'NotifyERRORIcon', { link = 'DiagnosticError' })
-          vim.api.nvim_set_hl(0, 'NotifyERRORTitle', { link = 'DiagnosticError' })
-          vim.api.nvim_set_hl(0, 'NotifyWARNBorder', { link = 'DiagnosticWarn' })
-          vim.api.nvim_set_hl(0, 'NotifyWARNIcon', { link = 'DiagnosticWarn' })
-          vim.api.nvim_set_hl(0, 'NotifyWARNTitle', { link = 'DiagnosticWarn' })
-          vim.api.nvim_set_hl(0, 'NotifyINFOBorder', { link = 'DiagnosticInfo' })
-          vim.api.nvim_set_hl(0, 'NotifyINFOIcon', { link = 'DiagnosticInfo' })
-          vim.api.nvim_set_hl(0, 'NotifyINFOTitle', { link = 'DiagnosticInfo' })
           local notify = require 'notify'
           notify.setup {
             fps = 120,

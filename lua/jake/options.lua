9558fd1dfcdacfb40efef8054b58345b41dabae2
jake jordan <jakesjordan11@gmail.com>
2023-03-06 21:50:33 -0700

    🔧 options tabupdate

--- lua/jake/options.lua
+++ lua/jake/options.lua
@@ -30,7 +30,7 @@ vim.opt.titlestring = '%t'
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.bo.softtabstop = 2
+vim.opt.softtabstop = 2
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
